4eabeb30f2879debe9928e80051d352a792386c9
Rishi-Bidani <rishibidani12@gmail.com>
2021-05-14 10:45:43 +0530

    commit before switch

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Home msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/Home.vue'
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
