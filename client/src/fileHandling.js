97fb33b61b078ec6969fcdc9bef76b2157bed4d2
Rishi-Bidani <rishibidani12@gmail.com>
2021-05-15 00:52:16 +0530

    requests to backend to fetch folder and files almost complete

--- client/src/fileHandling.js
+++ client/src/fileHandling.js
@@ -4,11 +4,18 @@ const url = "http://localhost:5000/posts/";
 class FileHandling {
   // Get Directories
   static getDirectories(forPath) {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
       try {
-        const res = await axios.get(`${url}/${forPath}`);
-        const fileAndFolders = res.data;
-        resolve(console.log(fileAndFolders));
+        console.log(forPath);
+        const fileAndFolders = axios
+          .post(`${url}/dir`, {
+            dir: forPath,
+          })
+          .then((response) => {
+            return response;
+          });
+        console.log(resolve(fileAndFolders));
+        return resolve(fileAndFolders);
       } catch (error) {
         reject(error);
       }
@@ -16,7 +23,7 @@ class FileHandling {
   }
   // Make Folder
   static newFolder(forPath) {
-    return axios.post(`${url}/${forPath}`);
+    return axios.post(`${url}/newFolder/${forPath}`);
   }
 }
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,6 +4,26 @@ const router = express.Router();
 const path = require("path");
 const uploadsFolder = path.join(__dirname, "../../uploads");
 
+// Function for finding folders and files in the directory
+async function FilesAndFoldersArgs(getpath) {
+  const promiseFolders = [];
+  const promiseFiles = [];
+  let files = await fs.promises.readdir(path.join(__dirname, "../../uploads", `${getpath}`), {
+    withFileTypes: true,
+  });
+  for (let file of files) {
+    if (file.isFile()) {
+      promiseFiles.push(file.name);
+    } else if (file.isDirectory()) {
+      promiseFolders.push(file.name);
+    }
+  }
+  return {
+    sendFiles: promiseFiles,
+    sendFolders: promiseFolders,
+  };
+}
+
 // Create a new Folder
 router.post("/newFolder", (req, res) => {
   let receivedPath = req.body.path;
@@ -17,6 +37,14 @@ router.post("/newFolder", (req, res) => {
     }
   });
   res.redirect("/");
+});
+
+// Get Directories
+router.post("/dir", async (req, res) => {
+  let dir = req.body.dir;
+  console.log(dir);
+  console.log(await FilesAndFoldersArgs(dir));
+  res.json(await FilesAndFoldersArgs(dir));
 });
 
 // Upload File
