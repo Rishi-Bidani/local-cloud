7b4a5b5eb7d9d6d8e3f123b658aba1c759130f80
Rishi-Bidani <rishibidani12@gmail.com>
2021-05-15 10:29:39 +0530

    working on side bar

--- server/main.js
+++ server/main.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 const cors = require("cors");
