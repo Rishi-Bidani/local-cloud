fb2445b4b383314b8d2b142b2225a86414dedd6c
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2022-05-24 01:55:01 +1000

    working on streaming the downloads for large files

--- client/src/js/requests.js
+++ client/src/js/requests.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import * as path from "path"
+import streamSaver from 'streamsaver'
+
 
 // Remove for production ------------------------------
 axios.defaults.baseURL = 'http://localhost:5000';
@@ -20,17 +22,41 @@ export default class Request {
     static async downloadFile(forPath, fileName) {
         console.log(forPath, fileName)
         try {
-            const res = await axios.get("gets/download-file", {
-                responseType: "arraybuffer",
-                params: {
-                    relPath: path.join(forPath, fileName)
-                },
-            })
-            const blob = new Blob([res.data]);
-            const link = document.createElement("a");
-            link.href = window.URL.createObjectURL(blob);
-            link.download = fileName;
-            link.click();
+            // const res = axios.get("gets/download-file", {
+            //     responseType: "arraybuffer",
+            //     params: {
+            //         relPath: path.join(forPath, fileName)
+            //     },
+            // })
+            // console.log(res);
+            // console.time("download");
+            // const blob = new Blob([await res.data]);
+            // console.timeLog("download")
+            // const link = document.createElement("a");
+            // link.href = window.URL.createObjectURL(blob);
+            // link.download = fileName;
+            // link.click();
+            // console.timeEnd("download");
+            const fileStream = streamSaver.createWriteStream(fileName);
+            const res = await fetch("gets/download-file?" + new URLSearchParams({
+                relPath: path.join(forPath, fileName)
+            }))
+            const readableStream = res.body
+            if (window.WritableStream && readableStream.pipeTo) {
+                return readableStream.pipeTo(fileStream)
+                    .then(() => console.log('done writing'))
+            }
+            window.writer = fileStream.getWriter()
+
+            const reader = res.body.getReader()
+            const pump = () => reader.read()
+                .then(res => res.done
+                    ? window.writer.close()
+                    : window.writer.write(res.value).then(pump))
+
+            pump()
+
+
         } catch (err) {
             console.log(err)
         }
