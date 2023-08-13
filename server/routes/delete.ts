4b617e4573a849919bb557892137065102fbfa0c
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2023-08-13 22:41:41 +1000

    added route for deleting folders

--- server/routes/delete.ts
+++ server/routes/delete.ts
@@ -44,4 +44,42 @@ router.delete("/", authenticator, async (req: express.Request, res: express.Resp
     }
 });
 
+router.delete("/folder", authenticator, async (req: express.Request, res: express.Response) => {
+    // get the path of the folder to delete
+    const pathname = req.body.pathname;
+    const baseFolder = (await settings).uploadfolder;
+    const fullPath = path.join(baseFolder, pathname);
+
+    Logging.delete(ApiType.DELETE, `deleting folder: ${fullPath}`);
+
+    try {
+        // check if user has permission to delete folder
+        const permissions = res.locals.permissions;
+        if (!permissions.canDeleteFile) {
+            throw new DeleteError(403, "You do not have permission to delete folders.");
+        }
+        // check if folder exists
+        const folderExists = await fs.stat(fullPath);
+        if (!folderExists.isDirectory()) {
+            throw new DeleteError(400, "Folder does not exist.");
+        }
+        // delete folder
+        await fs.rmdir(fullPath, { recursive: true });
+        return res.status(200).send("Folder deleted successfully.");
+    } catch (error) {
+        Logging.error(ApiType.DELETE, `error deleting folder: ${fullPath} - ${error}`);
+        // catch errors
+        // persmission errors
+        if (error instanceof DeleteError && error.httpCode === 403) {
+            return res.status(403).send("Permission Error: " + error.message);
+        }
+        // folder does not exist
+        if (error instanceof DeleteError && error.httpCode === 400) {
+            return res.status(400).send("Folder does not exist.");
+        }
+        // other errors
+        return res.status(500).send("Error deleting folder.");
+    }
+});
+
 export { router };
